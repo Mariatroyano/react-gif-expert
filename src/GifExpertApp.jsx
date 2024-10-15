21e46d85b30a7c5697430ab81e7b7597d9f1c262
Mariatroyano <maria2020troyano@gmail.com>
2024-10-15 16:41:36 -0500

    Primer commit

---
+++ src/GifExpertApp.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+import { useState } from "react";
+import { AddCategory } from "./components/AddCategory";
+import { GifGrid } from "./components/GifGrid";
+
+export const GifExpertApp = () => {
+  const [categories, setCategories] = useState(["One Pusch"]);
+
+  const onAddCategory = (newCategory) => {
+    if (categories.includes(newCategory)) return;
+
+    setCategories([newCategory, ...categories]);
+  };
+
+  return (
+    <>
+      <h1>GifExpertApp</h1>
+
+      <AddCategory onNewCategory={(event) => onAddCategory(event)} />
+
+      {categories.map((category) => (
+        <GifGrid key={category} category={category} />
+      ))}
+    </>
+  );
+};
